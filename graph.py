16b5c10552f24907d1d73ae9dd0a9c039ccdc82f
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2022-11-04 21:07:37 +0200

    Added sub_task_5.

--- graph.py
+++ graph.py
@@ -81,7 +81,10 @@ def del_edge(graph, edge):
     >>> del_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (2, 4))
     {1: [2, 5], 2: [1], 3: [4], 4: [3], 5: [1]}
     """
-    pass
+    v_1, v_2 = edge
+    graph[v_1].remove(v_2)
+    graph[v_2].remove(v_1)
+    return graph
 
 def add_node(graph, node):
     """ 
