e4bbff4925c5cf8ccf297b6a3f93b1021ec3b8d7
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2022-11-05 16:22:57 +0200

    Pylint changes for sub_task_5.

--- graph.py
+++ graph.py
@@ -80,11 +80,9 @@ def add_edge(graph, edge):
     return graph
 
 def del_edge(graph, edge):
-    """ 
+    """
     (dict, tuple) -> (dict)
-    
     Delete an edge from the graph and return a new graph.
-    
     >>> del_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (2, 4))
     {1: [2, 5], 2: [1], 3: [4], 4: [3], 5: [1]}
     >>> del_edge({1: [2], 2: [1]}, (1, 3))
@@ -95,7 +93,7 @@ def del_edge(graph, edge):
     {1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}
     """
     v_1, v_2 = edge
-    if v_1 in graph and v_2 in graph and v_2 in graph[v_1] and v_1 in graph[v_2]:    
+    if v_1 in graph and v_2 in graph and v_2 in graph[v_1] and v_1 in graph[v_2]:
         graph[v_1].remove(v_2)
         graph[v_2].remove(v_1)
     return graph
