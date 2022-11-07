9219c7a580dfc33d243f44a468df93e4e1b57528
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2022-11-07 02:19:41 +0200

    Changes for sub_task_2

--- graph.py
+++ graph.py
@@ -17,11 +17,9 @@ def get_graph_from_file(file_name):
     return graph
 
 def to_edge_dict(edge_list):
-    """ 
+    """
     (list) -> (dict)
-
     Convert a graph from list of edges to dictionary of vertices.
-    
     >>> to_edge_dict([[1, 2], [3, 4], [1, 5], [2, 4]])
     {1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}
     """
